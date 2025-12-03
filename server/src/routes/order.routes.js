e3fba9cdf019d730031732438a3fcbbe7a14cfaf
DuyVu4w <duyvu4w@gmail.com>
2025-12-03 15:21:40 +0700

    test fetch order his

--- server/src/routes/order.routes.js
+++ server/src/routes/order.routes.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const orderController = require('../controllers/order.controller')
+const verifyToken = require('../middleware/auth.middleware');
 
 router.post('/addOrder', orderController.addOrder)
-
+router.get('/my-orders', verifyToken, orderController.getMyOrders);
+router.get('/my-order-test/:userId', orderController.getMyOrdersTest)
 module.exports = router
